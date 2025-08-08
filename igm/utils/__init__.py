5a0510d2e0fbd5c7000908980dda1b0e45bfb59f
Lorenzo Boninsegna <lorenzobonimba@gmail.com>
2025-08-08 09:31:31 -0700

    remove old files

--- igm/utils/__init__.py
+++ igm/utils/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import division, print_function
 
-from .actdist import get_actdist
+#from .actdist import get_actdist
 from .files import HmsFile, VolumeFile
 
 from .log import SetupLogging, logger
