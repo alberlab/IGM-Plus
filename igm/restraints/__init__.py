b972d05276079e0620dbd483de64110a5e4ebfc3
Lorenzo Boninsegna <lorenzobonimba@gmail.com>
2025-08-13 16:47:26 -0700

    remove deprecated comprehensive HiC restraint, we now distinguish inter and intra

--- igm/restraints/__init__.py
+++ igm/restraints/__init__.py
@@ -3,7 +3,6 @@ from __future__ import division, print_function
 from .envelope    import Envelope
 from .polymer     import Polymer
 from .steric      import Steric
-from .hic         import HiC
 from .sprite      import Sprite
 from .damid       import Damid
 from .genenvelope import GenEnvelope
